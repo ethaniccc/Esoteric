f82bd55b1382fcc217b0d20a913dec91ef406299
ethaniccc <schooldocumentsandstuff@gmail.com>
2021-04-14 16:07:10 -0400

    remove useless files

--- versai_eu.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cd ..
-php -dphar.readonly=0 DevTools.phar --make Esoteric --entry Esoteric/src/ethaniccc/Esoteric/Esoteric.php --out Esoteric.phar
-cd Esoteric/upload
-php Upload.php
-cd ..
