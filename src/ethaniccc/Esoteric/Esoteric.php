d3507b233bb8aa37ba0ca9bb03e42e8e7015d7e6
ethaniccc <schooldocumentsandstuff@gmail.com>
2021-05-11 12:01:02 -0400

    start to redo everything

--- src/ethaniccc/Esoteric/Esoteric.php
+++ src/ethaniccc/Esoteric/Esoteric.php
@@ -2,15 +2,7 @@
 
 namespace ethaniccc\Esoteric;
 
-use ethaniccc\Esoteric\command\EsotericCommand;
-use ethaniccc\Esoteric\data\PlayerData;
-use ethaniccc\Esoteric\data\PlayerDataManager;
-use ethaniccc\Esoteric\listener\Listener;
 use ethaniccc\Esoteric\protocol\v428\PlayerAuthInputPacket;
-use ethaniccc\Esoteric\tasks\CreateBanwaveTask;
-use ethaniccc\Esoteric\tasks\TickingTask;
-use ethaniccc\Esoteric\thread\LoggerThread;
-use ethaniccc\Esoteric\utils\banwave\Banwave;
 use Exception;
 use pocketmine\event\HandlerListManager;
 use pocketmine\network\mcpe\protocol\PacketPool;
@@ -31,22 +23,6 @@ final class Esoteric {
 	public $plugin;
 	/** @var Settings */
 	public $settings;
-	/** @var LoggerThread */
-	public $logger;
-	/** @var Listener */
-	public $listener;
-	/** @var PlayerData[] */
-	public $hasAlerts = [];
-	/** @var PlayerDataManager */
-	public $dataManager;
-	/** @var string[] */
-	public $logCache = [];
-	/** @var TickingTask */
-	public $tickingTask;
-	/** @var Banwave|null */
-	public $banwave;
-	/** @var EsotericCommand */
-	public $command;
 
 	/**
 	 * Esoteric constructor.
@@ -56,10 +32,6 @@ final class Esoteric {
 	private function __construct(PluginBase $plugin, ?Config $config) {
 		$this->plugin = $plugin;
 		$this->settings = new Settings($config->getAll());
-		$this->logger = new LoggerThread($this->getPlugin()->getDataFolder() . "esoteric.log");
-		$this->listener = new Listener();
-		$this->dataManager = new PlayerDataManager();
-		$this->tickingTask = new TickingTask();
 	}
 
 	/**
@@ -80,30 +52,7 @@ final class Esoteric {
 		if ($this->running)
 			return;
 
-		$this->logger->start(PTHREADS_INHERIT_NONE);
-		$this->plugin->getServer()->getPluginManager()->registerEvents($this->listener, $this->plugin);
-		$this->plugin->getScheduler()->scheduleRepeatingTask($this->tickingTask, 1);
-		if ($this->settings->getWaveSettings()["enabled"]) {
-			@mkdir($this->getPlugin()->getDataFolder() . "banwaves");
-			$count = count(scandir($this->getPlugin()->getDataFolder() . "banwaves")) - 2;
-			if ($count === 0) {
-				Server::getInstance()->getAsyncPool()->submitTask(new CreateBanwaveTask($this->getPlugin()->getDataFolder() . "banwaves/banwave-1.json", function (Banwave $banwave): void {
-					$this->banwave = $banwave;
-				}));
-			} else {
-				$filtered = array_filter(scandir($this->getPlugin()->getDataFolder() . "banwaves"), static function (string $file): bool {
-					return strtolower(($array = explode(".", $file))[count($array) - 1]) === "json";
-				});
-				Server::getInstance()->getAsyncPool()->submitTask(new CreateBanwaveTask($this->getPlugin()->getDataFolder() . "banwaves/" . $filtered[max(array_keys($filtered))], function (Banwave $banwave): void {
-					$this->banwave = $banwave;
-				}));
-			}
-		}
-		$this->command = new EsotericCommand();
-		Server::getInstance()->getCommandMap()->register($this->plugin->getName(), $this->command);
 		PacketPool::getInstance()->registerPacket(new PlayerAuthInputPacket());
-
-		$this->running = true;
 	}
 
 	public function getPlugin(): PluginBase {
@@ -118,20 +67,11 @@ final class Esoteric {
 	}
 
 	public function stop(): void {
-		$this->logger->quit();
-		HandlerListManager::global()->unregisterAll($this->listener);
-		Server::getInstance()->getCommandMap()->unregister($this->command);
-		$this->tickingTask->getHandler()->cancel();
-
 		$this->running = false;
 	}
 
 	public function getSettings(): Settings {
 		return $this->settings;
-	}
-
-	public function getBanwave(): ?Banwave {
-		return $this->banwave;
 	}
 
 }
---
+++ src/ethaniccc/Esoteric/thread/EsotericThread.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ethaniccc\Esoteric\thread;
+
+use ethaniccc\Esoteric\protocol\v428\PlayerAuthInputPacket;
+use pocketmine\network\mcpe\protocol\PacketPool;
+use pocketmine\thread\Thread;
+use Threaded;
+use function microtime;
+use function usleep;
+
+class EsotericThread extends Thread {
+
+	private const TICKS_PER_SECOND = 40;
+
+	public $inboundQueue;
+	public $outboundQueue;
+	public $sendPacketQueue;
+
+	public $networkStackLatencyHandler;
+	public $dataManager;
+
+	private static $instance;
+
+	public static function getInstance(): self {
+		return self::$instance;
+	}
+
+	public function __construct() {
+		$this->setClassLoader();
+	}
+
+	protected function onRun(): void {
+		self::$instance = $this;
+		$this->inboundQueue = new Threaded();
+		$this->outboundQueue = new Threaded();
+		$this->sendPacketQueue = new Threaded();
+		$this->registerClassLoader();
+		PacketPool::getInstance()->registerPacket(new PlayerAuthInputPacket());
+		while (!$this->isKilled) {
+			$start = microtime(true);
+			$this->tick();
+			$delta = microtime(true) - $start;
+			if ($delta > (1 / self::TICKS_PER_SECOND)) {
+
+			} else {
+				usleep(1000000 / self::TICKS_PER_SECOND);
+			}
+		}
+	}
+
+	private function tick(): void {
+
+	}
+
+}
