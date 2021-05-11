429d73e310c954f3588326d00563600324d55b2e
ethaniccc <schooldocumentsandstuff@gmail.com>
2021-05-11 19:10:38 -0400

    raptor I love you omfg

--- src/ethaniccc/Esoteric/utils/ChunkDeserializer.php
+++ src/ethaniccc/Esoteric/utils/ChunkDeserializer.php
@@ -2,50 +2,48 @@
 
 namespace ethaniccc\Esoteric\utils;
 
-use Exception;
 use pocketmine\block\BlockLegacyIds;
-use pocketmine\data\bedrock\LegacyBlockIdToStringIdMap;
-use pocketmine\nbt\BaseNbtSerializer;
-use pocketmine\nbt\LittleEndianNbtSerializer;
 use pocketmine\utils\BinaryStream;
+use pocketmine\world\format\BiomeArray;
 use pocketmine\world\format\Chunk;
-use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
-use pocketmine\world\format\io\exception\CorruptedChunkException;
-use pocketmine\world\format\SubChunk;
 use pocketmine\world\format\PalettedBlockArray;
-use function var_dump;
+use pocketmine\world\format\SubChunk;
 
 final class ChunkDeserializer {
 
-	public static function deserialize(string $data, int $subChunkCount, int $chunkX, int $chunkZ): Chunk {
-		return new Chunk();
+	public static function deserialize(string $payload, int $subChunkCount) : Chunk{
+		$stream = new BinaryStream($payload);
+
+		$subChunks = [];
+		for($y = 0; $y < $subChunkCount; ++$y){
+			$stream->getByte(); //version
+			$layers = [];
+			for($l = 0, $layerCount = $stream->getByte(); $l < $layerCount; ++$l){
+				$layers[] = self::deserializePalettedBlockArray($stream);
+			}
+			$subChunks[$y] = new SubChunk(BlockLegacyIds::AIR << 4, $layers);
+		}
+
+		$biomeIdArray = $stream->get(256);
+
+		$stream->getByte(); //border block array count
+
+		// TODO: tiles
+
+		return new Chunk($subChunks, null, null, new BiomeArray($biomeIdArray));
 	}
 
-	public static function deserializePaletted(BinaryStream $stream): PalettedBlockArray {
+	public static function deserializePalettedBlockArray(BinaryStream $stream) : PalettedBlockArray{
 		$bitsPerBlock = $stream->getByte() >> 1;
-		try{
-			$words = $stream->get(PalettedBlockArray::getExpectedWordArraySize($bitsPerBlock));
-		}catch(\InvalidArgumentException $e){
-			throw new CorruptedChunkException("Failed to deserialize paletted storage: " . $e->getMessage(), 0, $e);
-		}
-		$nbt = new LittleEndianNbtSerializer();
-		$palette = [];
-		$idMap = LegacyBlockIdToStringIdMap::getInstance();
-		for($i = 0, $paletteSize = $stream->getLInt(); $i < $paletteSize; ++$i){
-			$offset = $stream->getOffset();
-			$tag = $nbt->read($stream->getBuffer(), $offset)->mustGetCompoundTag();
-			$stream->setOffset($offset);
 
-			$id = $idMap->stringToLegacy($tag->getString("name")) ?? BlockLegacyIds::INFO_UPDATE;
-			$data = $tag->getShort("val");
-			if($id === BlockLegacyIds::AIR){
-				//TODO: quick and dirty hack for artifacts left behind by broken world editors
-				//we really need a proper state fixer, but this is a pressing issue.
-				$data = 0;
-			}
-			$palette[] = ($id << 4) | $data;
+		$wordArray = $stream->get(PalettedBlockArray::getExpectedWordArraySize($bitsPerBlock));
+
+		$palette = [];
+		for($i = 0, $paletteCount = $stream->getVarInt(); $i < $paletteCount; ++$i){
+			$palette[$i] = $stream->getVarInt();
 		}
-		return PalettedBlockArray::fromData($bitsPerBlock, $words, $palette);
+
+		return PalettedBlockArray::fromData($bitsPerBlock, $wordArray, $palette);
 	}
 
 }
