7b895f2e95d1972186046627039bb0b3b7e43e8f
ethaniccc <schooldocumentsandstuff@gmail.com>
2021-05-11 12:10:31 -0400

    debug log because conflict

--- src/ethaniccc/Esoteric/thread/EsotericThread.php
+++ src/ethaniccc/Esoteric/thread/EsotericThread.php
@@ -5,6 +5,7 @@ namespace ethaniccc\Esoteric\thread;
 use ethaniccc\Esoteric\protocol\v428\PlayerAuthInputPacket;
 use pocketmine\network\mcpe\protocol\PacketPool;
 use pocketmine\thread\Thread;
+use pocketmine\utils\MainLogger;
 use Threaded;
 use function microtime;
 use function usleep;
@@ -20,14 +21,17 @@ class EsotericThread extends Thread {
 	public $networkStackLatencyHandler;
 	public $dataManager;
 
+	private $logger;
+
 	private static $instance;
 
 	public static function getInstance(): self {
 		return self::$instance;
 	}
 
-	public function __construct() {
+	public function __construct(MainLogger $logger) {
 		$this->setClassLoader();
+		$this->logger = $logger;
 	}
 
 	protected function onRun(): void {
@@ -42,7 +46,7 @@ class EsotericThread extends Thread {
 			$this->tick();
 			$delta = microtime(true) - $start;
 			if ($delta > (1 / self::TICKS_PER_SECOND)) {
-
+				$this->logger->debug("delta=$delta, Esoteric thread not sleeping");
 			} else {
 				usleep(1000000 / self::TICKS_PER_SECOND);
 			}
